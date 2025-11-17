c7e5b3c5eeb52d8f515466e7a492e98f2ace20da
nagadhanush700 <nagadhanush700@gmail.com>
2025-11-17 20:35:34 +0530

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,8 @@
+import math
+num = float(input("Enter a number: "))
+sqrt_value = math.sqrt(num)
+log_value = math.log(num)     
+sine_value = math.sin(num)     
+print("Square root:", sqrt_value)
+print("Natural logarithm (ln):", log_value)
+print("Sine (in radians):", sine_value)
